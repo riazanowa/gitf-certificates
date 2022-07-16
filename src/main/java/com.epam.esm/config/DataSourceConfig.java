d183ab3c92bc246ebd8933e791bb11849391ba8b
Ekaterina Ryazanova <kat.riazanowa@yandex.ru>
2022-07-16 17:51:30 +0300

    db configuration from external properties file

--- src/main/java/com.epam.esm/config/DataSourceConfig.java
+++ src/main/java/com.epam.esm/config/DataSourceConfig.java
@@ -1,8 +1,10 @@
 package com.epam.esm.config;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
@@ -10,13 +12,20 @@ import javax.sql.DataSource;
 @Configuration
 public class DataSourceConfig {
 
+    private final Environment environment;
+
+    @Autowired
+    public DataSourceConfig(Environment environment) {
+        this.environment = environment;
+    }
+
     @Bean
     public DataSource dataSource() {
         BasicDataSource bds = new BasicDataSource();
-        bds.setDriverClassName("org.postgresql.Driver");
-        bds.setUrl("jdbc:postgresql://localhost:5432/postgres");
-        bds.setUsername("postgres");
-        bds.setPassword("postgres");
+        bds.setDriverClassName(environment.getProperty("driver"));
+        bds.setUrl(environment.getProperty("url"));
+        bds.setUsername(environment.getProperty("user"));
+        bds.setPassword(environment.getProperty("password"));
         bds.setInitialSize(5);
         return bds;
     }
--- src/main/java/com.epam.esm/config/WebConfig.java
+++ src/main/java/com.epam.esm/config/WebConfig.java
@@ -4,19 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan("com.epam.esm")
+@ComponentScan("com")
+@PropertySource("classpath:database.properties")
 public class WebConfig implements WebMvcConfigurer {
 
+    private final Environment environment;
     private final ApplicationContext applicationContext;
 
     @Autowired
-    public WebConfig(ApplicationContext applicationContext) {
+    public WebConfig(Environment environment, ApplicationContext applicationContext) {
+        this.environment = environment;
         this.applicationContext = applicationContext;
     }
 
